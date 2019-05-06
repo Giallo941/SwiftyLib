ed3eadcceda3cae9ab0132ac157e972d10c94383
Gianmarco Cotellessa <g.cotellessa@betacom.it>
2019-05-06 16:24:58 +0200

    Initial Commit

---
+++ SwiftyLib/SwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLib.h
+//  SwiftyLib
+//
+//  Created by Gianmarco Cotellessa on 06/05/2019.
+//  Copyright © 2019 Gianmarco Cotellessa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyLib.
+FOUNDATION_EXPORT double SwiftyLibVersionNumber;
+
+//! Project version string for SwiftyLib.
+FOUNDATION_EXPORT const unsigned char SwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib/PublicHeader.h>
+
+
